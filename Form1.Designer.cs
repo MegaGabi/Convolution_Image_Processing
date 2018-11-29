7b2155c588ec17d8ba1aa31abef06d43b6a5dcaa
Artem <megagabi@ukr.net>
2018-11-29 20:38:04 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace convo_test
+{
+    partial class wnd_Convo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pb_ToInsert = new System.Windows.Forms.PictureBox();
+            this.pb_BwImage = new System.Windows.Forms.PictureBox();
+            this.pb_ConvoRes = new System.Windows.Forms.PictureBox();
+            this.btn_Slash = new System.Windows.Forms.Button();
+            this.btn_reverseSlash = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_StraightLine = new System.Windows.Forms.Button();
+            this.btn_HorizontalLine = new System.Windows.Forms.Button();
+            this.btn_Star = new System.Windows.Forms.Button();
+            this.pb_Blured = new System.Windows.Forms.PictureBox();
+            this.btn_Cross = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_ToInsert)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_BwImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_ConvoRes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Blured)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb_ToInsert
+            // 
+            this.pb_ToInsert.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pb_ToInsert.Location = new System.Drawing.Point(12, 12);
+            this.pb_ToInsert.Name = "pb_ToInsert";
+            this.pb_ToInsert.Size = new System.Drawing.Size(258, 272);
+            this.pb_ToInsert.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_ToInsert.TabIndex = 0;
+            this.pb_ToInsert.TabStop = false;
+            // 
+            // pb_BwImage
+            // 
+            this.pb_BwImage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pb_BwImage.Location = new System.Drawing.Point(276, 12);
+            this.pb_BwImage.Name = "pb_BwImage";
+            this.pb_BwImage.Size = new System.Drawing.Size(258, 272);
+            this.pb_BwImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_BwImage.TabIndex = 1;
+            this.pb_BwImage.TabStop = false;
+            // 
+            // pb_ConvoRes
+            // 
+            this.pb_ConvoRes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pb_ConvoRes.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pb_ConvoRes.Location = new System.Drawing.Point(804, 12);
+            this.pb_ConvoRes.Name = "pb_ConvoRes";
+            this.pb_ConvoRes.Size = new System.Drawing.Size(258, 272);
+            this.pb_ConvoRes.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_ConvoRes.TabIndex = 2;
+            this.pb_ConvoRes.TabStop = false;
+            // 
+            // btn_Slash
+            // 
+            this.btn_Slash.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_Slash.Location = new System.Drawing.Point(8, 306);
+            this.btn_Slash.Name = "btn_Slash";
+            this.btn_Slash.Size = new System.Drawing.Size(75, 23);
+            this.btn_Slash.TabIndex = 3;
+            this.btn_Slash.Text = "/";
+            this.btn_Slash.UseVisualStyleBackColor = true;
+            this.btn_Slash.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn_reverseSlash
+            // 
+            this.btn_reverseSlash.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_reverseSlash.Location = new System.Drawing.Point(8, 335);
+            this.btn_reverseSlash.Name = "btn_reverseSlash";
+            this.btn_reverseSlash.Size = new System.Drawing.Size(75, 23);
+            this.btn_reverseSlash.TabIndex = 4;
+            this.btn_reverseSlash.Text = "\\";
+            this.btn_reverseSlash.UseVisualStyleBackColor = true;
+            this.btn_reverseSlash.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 287);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Convolutional Filters";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btn_StraightLine
+            // 
+            this.btn_StraightLine.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_StraightLine.Location = new System.Drawing.Point(8, 364);
+            this.btn_StraightLine.Name = "btn_StraightLine";
+            this.btn_StraightLine.Size = new System.Drawing.Size(75, 23);
+            this.btn_StraightLine.TabIndex = 6;
+            this.btn_StraightLine.Text = "|";
+            this.btn_StraightLine.UseVisualStyleBackColor = true;
+            this.btn_StraightLine.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn_HorizontalLine
+            // 
+            this.btn_HorizontalLine.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_HorizontalLine.Location = new System.Drawing.Point(8, 393);
+            this.btn_HorizontalLine.Name = "btn_HorizontalLine";
+            this.btn_HorizontalLine.Size = new System.Drawing.Size(75, 23);
+            this.btn_HorizontalLine.TabIndex = 7;
+            this.btn_HorizontalLine.Text = "-";
+            this.btn_HorizontalLine.UseVisualStyleBackColor = true;
+            this.btn_HorizontalLine.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn_Star
+            // 
+            this.btn_Star.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_Star.Location = new System.Drawing.Point(89, 335);
+            this.btn_Star.Name = "btn_Star";
+            this.btn_Star.Size = new System.Drawing.Size(75, 23);
+            this.btn_Star.TabIndex = 8;
+            this.btn_Star.Text = "*";
+            this.btn_Star.UseVisualStyleBackColor = true;
+            this.btn_Star.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // pb_Blured
+            // 
+            this.pb_Blured.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pb_Blured.Location = new System.Drawing.Point(540, 12);
+            this.pb_Blured.Name = "pb_Blured";
+            this.pb_Blured.Size = new System.Drawing.Size(258, 272);
+            this.pb_Blured.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_Blured.TabIndex = 9;
+            this.pb_Blured.TabStop = false;
+            // 
+            // btn_Cross
+            // 
+            this.btn_Cross.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_Cross.Location = new System.Drawing.Point(89, 306);
+            this.btn_Cross.Name = "btn_Cross";
+            this.btn_Cross.Size = new System.Drawing.Size(75, 23);
+            this.btn_Cross.TabIndex = 10;
+            this.btn_Cross.Text = "+";
+            this.btn_Cross.UseVisualStyleBackColor = true;
+            this.btn_Cross.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // wnd_Convo
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Window;
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1076, 430);
+            this.Controls.Add(this.btn_Cross);
+            this.Controls.Add(this.pb_Blured);
+            this.Controls.Add(this.btn_Star);
+            this.Controls.Add(this.btn_HorizontalLine);
+            this.Controls.Add(this.btn_StraightLine);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_reverseSlash);
+            this.Controls.Add(this.btn_Slash);
+            this.Controls.Add(this.pb_ConvoRes);
+            this.Controls.Add(this.pb_BwImage);
+            this.Controls.Add(this.pb_ToInsert);
+            this.MinimumSize = new System.Drawing.Size(1092, 469);
+            this.Name = "wnd_Convo";
+            this.Text = "Form1";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.pb_ToInsert_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.pb_ToInsert_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_ToInsert)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_BwImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_ConvoRes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Blured)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb_ToInsert;
+        private System.Windows.Forms.PictureBox pb_BwImage;
+        private System.Windows.Forms.PictureBox pb_ConvoRes;
+        private System.Windows.Forms.Button btn_Slash;
+        private System.Windows.Forms.Button btn_reverseSlash;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_StraightLine;
+        private System.Windows.Forms.Button btn_HorizontalLine;
+        private System.Windows.Forms.Button btn_Star;
+        private System.Windows.Forms.PictureBox pb_Blured;
+        private System.Windows.Forms.Button btn_Cross;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace convo_test
+{
+    public partial class wnd_Convo : Form
+    {
+        enum ConvoArrayNames { ReverseSlash, Slash, VerticalLine, HorizontalLine, Star, Cross };
+        int[][,] Convolution = new int[][,]
+        {
+            new int[,]
+            {
+                 {  0,  1, 2},
+                 { -1,  0, 1},
+                 { -2, -1, 0},
+            },
+            new int[,]
+            {
+                 { 2,  1,  0},
+                 { 1,  0, -1},
+                 { 0, -1, -2},
+            },
+            new int[,]
+            {
+                 { -1, 0, 1},
+                 { -2, 0, 2},
+                 { -1, 0, 1},
+            },
+            new int[,]
+            {
+                 { -1, -2, -1},
+                 {  0,  0,  0},
+                 {  1,  2,  1},
+            },
+            new int[,]
+            {
+                 { -1, -1, -1},
+                 { -1,  8, -1},
+                 { -1, -1, -1},
+            },
+            new int[,]
+            {
+                 {  0, -1,  0},
+                 { -1,  4, -1},
+                 {  0, -1,  0},
+            }
+        };
+
+        double[,] GaussianK;
+
+        private double[,] GenerateGaussianKernel(double theta, int KernelSize)
+        {
+            double[,] GaussianKernel = new double[KernelSize, KernelSize];
+
+            for (int i = 0; i < KernelSize; i++)
+            {
+                for (int j = 0; j < KernelSize; j++)
+                {
+                    int By = (KernelSize / 2) - i;
+                    int Bx = -(KernelSize / 2) + j;
+
+                    double GaussianBlurValue =
+                        1 / (2 * Math.PI * Math.Pow(theta, 2))
+                        * Math.Pow(Math.E, -(Math.Pow(Bx, 2) + Math.Pow(By, 2)) / (2 * Math.Pow(theta, 2)));
+
+                    GaussianKernel[i, j] = GaussianBlurValue;
+                }
+            }
+
+            return GaussianKernel;
+        }
+
+        public wnd_Convo()
+        {
+            InitializeComponent();
+
+            GaussianK = GenerateGaussianKernel(1.4, 5);
+        }
+
+        private void pb_ToInsert_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void pb_ToInsert_DragDrop(object sender, DragEventArgs e)
+        {
+            if (pb_ToInsert.ClientRectangle.Contains(pb_ToInsert.PointToClient(Control.MousePosition)))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                if (files.Length != 0)
+                {
+                    string picture = files[0];
+
+                    Bitmap bmp = new Bitmap(Image.FromFile(picture), pb_ToInsert.Size.Width, pb_ToInsert.Size.Height);
+                    pb_ToInsert.Image = bmp;
+
+                    pb_BwImage.Image = ImageToBW(pb_ToInsert.Image);
+
+                    Bitmap b = new Bitmap(pb_BwImage.Image);
+                    GaussianBlur(ref b, GaussianK);
+
+                    pb_Blured.Image = b;
+                }
+            }
+        }
+
+        private Bitmap AddTwoBWImages(Bitmap f, Bitmap s)
+        {
+            if (f.Size != s.Size)
+                return null;
+
+            Bitmap res = new Bitmap(f.Width, f.Height);
+
+            for(int i = 0; i < res.Height; i++)
+            {
+                for (int j = 0; j < res.Width; j++)
+                {
+                    int rgb = (int)Math.Sqrt((Math.Pow(f.GetPixel(j,i).R, 2) + Math.Pow(s.GetPixel(j, i).R, 2)));
+                    rgb = (rgb > 255 ? 255 : rgb);
+                    res.SetPixel(j,i,Color.FromArgb(rgb, rgb, rgb));
+                }
+            }
+
+            return res;
+        }
+
+        private void NonMaximumSupression(ref Bitmap toCompute, int maskSize)
+        {
+            for (int y = 0; y < toCompute.Height; y++)
+            {
+                for (int x = 0; x < toCompute.Width; x++)
+                {
+                    int maximum = 0;
+
+                    for (int i = 0; i < maskSize; i++)
+                    {
+                        for (int j = 0; j < maskSize; j++)
+                        {
+                            if (x + j < toCompute.Width && y + i < toCompute.Height)
+                            {
+                                if (toCompute.GetPixel(x + j, y + i).R > maximum)
+                                {
+                                    maximum = toCompute.GetPixel(x + j, y + i).R;
+                                }
+                            }
+                        }
+                    }
+
+                    for (int i = 0; i < maskSize; i++)
+                    {
+                        for (int j = 0; j < maskSize; j++)
+                        {
+                            if (x + j < toCompute.Width && y + i < toCompute.Height)
+                            {
+                                if (toCompute.GetPixel(x + j, y + i).R != maximum)
+                                {
+                                    toCompute.SetPixel(x + j, y + i, Color.Black);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } //бред
+
+        private Image ImageToBW(Image img)
+        {
+            Bitmap res = new Bitmap(img);
+
+            for(int x = 0; x < res.Width; x++)
+            {
+                for (int y = 0; y < res.Height; y++)
+                {
+                    Color clr = res.GetPixel(x, y);
+
+                    int rgb = (clr.R + clr.G + clr.B) / 3;
+                    res.SetPixel(x, y, Color.FromArgb(rgb, rgb, rgb));
+                }
+            }
+
+            return res;
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            if (pb_BwImage.Image == null) return;
+
+            Bitmap imgToProcess = new Bitmap(pb_Blured.Image);
+
+            
+            if (sender == btn_reverseSlash)
+            {
+                BackgroundConvolution(imgToProcess, 
+                            ((object s, DoWorkEventArgs ret) =>
+                             ret.Result = ApplyConvToPicture(imgToProcess, Convolution[(int)ConvoArrayNames.ReverseSlash])
+                            ));
+            }
+            else if (sender == btn_Slash)
+            {
+                BackgroundConvolution(imgToProcess,
+                            ((object s, DoWorkEventArgs ret) =>
+                             ret.Result = ApplyConvToPicture(imgToProcess, Convolution[(int)ConvoArrayNames.Slash])
+                            ));
+            }
+            else if (sender == btn_StraightLine)
+            {
+                BackgroundConvolution(imgToProcess,
+                            ((object s, DoWorkEventArgs ret) =>
+                             ret.Result = ApplyConvToPicture(imgToProcess, Convolution[(int)ConvoArrayNames.VerticalLine])
+                            ));
+            }
+            else if (sender == btn_HorizontalLine)
+            {
+                BackgroundConvolution(imgToProcess,
+                            ((object s, DoWorkEventArgs ret) =>
+                             ret.Result = ApplyConvToPicture(imgToProcess, Convolution[(int)ConvoArrayNames.HorizontalLine])
+                            ));
+            }
+            else if (sender == btn_Star)
+            {
+                BackgroundConvolution(imgToProcess,
+                            ((object s, DoWorkEventArgs ret) =>
+                            {
+                                Bitmap HorizontalResult = ApplyConvToPicture(imgToProcess, Convolution[(int)ConvoArrayNames.HorizontalLine]);
+                                Bitmap VerticalResult = ApplyConvToPicture(imgToProcess, Convolution[(int)ConvoArrayNames.VerticalLine]);
+
+                                Bitmap SlashResult = ApplyConvToPicture(imgToProcess, Convolution[(int)ConvoArrayNames.Slash]);
+                                Bitmap ReverseSlashResult = ApplyConvToPicture(imgToProcess, Convolution[(int)ConvoArrayNames.ReverseSlash]);
+
+                                Bitmap cross = AddTwoBWImages(HorizontalResult, VerticalResult);
+                                Bitmap x = AddTwoBWImages(SlashResult, ReverseSlashResult);
+
+                                ret.Result = AddTwoBWImages(cross, x);
+                            }
+                            ));
+            }
+            else if (sender == btn_Cross)
+            {
+                BackgroundConvolution(imgToProcess,
+                            ((object s, DoWorkEventArgs ret) =>
+                            {
+                                Bitmap HorizontalResult = ApplyConvToPicture(imgToProcess, Convolution[(int)ConvoArrayNames.HorizontalLine]);
+                                Bitmap VerticalResult = ApplyConvToPicture(imgToProcess, Convolution[(int)ConvoArrayNames.VerticalLine]);
+
+                                ret.Result = AddTwoBWImages(HorizontalResult, VerticalResult);
+                            }
+                            ));
+            }
+        }
+
+        private void BackgroundConvolution(Bitmap imgToProcess, DoWorkEventHandler method)
+        {
+            BackgroundWorker bw = new BackgroundWorker();
+            bw.DoWork += method;
+            bw.RunWorkerCompleted += ((object sender, RunWorkerCompletedEventArgs e) => pb_ConvoRes.Image = new Bitmap((Bitmap)e.Result));
+            bw.RunWorkerAsync();
+        }
+
+        private Bitmap ApplyConvToPicture(Bitmap imgToProcess, int[,] v)
+        {
+            Bitmap res = new Bitmap(imgToProcess.Width / v.GetLength(1) + 1, imgToProcess.Height / v.GetLength(0) + 1);
+
+            int brd = v.GetLength(0) / 2;
+            imgToProcess = new Bitmap(imgToProcess, imgToProcess.Width + v.GetLength(1), imgToProcess.Height + v.GetLength(0));
+
+            for (int y = 0; y < imgToProcess.Height - brd - 1; y++)
+            {
+                for (int x = 0; x < imgToProcess.Width - brd - 1; x++)
+                {
+                    int rgb = 0;
+                    //конволюция
+                    for(int i = 0; i < v.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < v.GetLength(1); j++)
+                        {
+                            rgb += imgToProcess.GetPixel(x + j, y + i).R * v[i,j];
+                        }
+                    }
+                    rgb /= v.Length;
+
+                    rgb %= 255;
+                    rgb = rgb < 0 ? 0 : rgb;
+
+                    res.SetPixel(x / v.GetLength(1), y / v.GetLength(0), Color.FromArgb(rgb, rgb, rgb));
+                }
+            }
+
+            //NonMaximumSupression(ref res, 5);
+
+            return res;
+        }
+
+        private void GaussianBlur(ref Bitmap toBlur, double[,] Kernel)
+        {
+            Bitmap res = new Bitmap(toBlur);
+
+            int GKernelY = Kernel.GetLength(0);
+            int GKernelX = Kernel.GetLength(1);
+
+            int gaussianSize = GKernelY * GKernelX;
+
+            for (int y = 0; y < toBlur.Height; y++)
+            {
+                for (int x = 0; x < toBlur.Width; x++)
+                {
+                    double rgb = 0;
+                    for (int i = 0; i < GKernelY; i++)
+                    {
+                        for (int j = 0; j < GKernelX; j++)
+                        {
+                            int XtoGet = x + j >= toBlur.Width ? toBlur.Width - 1 : x + j;
+                            int YtoGet = y + i >= toBlur.Height ? toBlur.Height - 1 : y + i;
+
+                            rgb += toBlur.GetPixel(XtoGet, YtoGet).R * Kernel[i,j];
+                        }
+                    }
+
+                    //rgb *= (blurSize / 1.5);
+                    rgb = rgb > 255 ? 255 : rgb;
+
+                    res.SetPixel(x, y, Color.FromArgb((int)rgb, (int)rgb, (int)rgb));
+               }
+            }
+            toBlur = res;
+       }
+    }
+}
